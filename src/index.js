c3bb4affea589ec7e263dbc2327f3932b4c2234e
Guifamelo <guilhermefdemelo@hotmail.com>
2023-05-13 02:44:08 -0300

    Cors fixing

--- src/index.js
+++ src/index.js
@@ -22,8 +22,9 @@ app.use(express.json())
 
 
 
-app.use(routes)
 
 app.use(cors())
 
-app.listen(3333, () => console.log('Server running'))
+app.use(routes)
+
+app.listen(3333 , () => console.log('Server running'))
